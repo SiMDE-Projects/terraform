1a0240711d9db2cac36ae3b2a770bdea235c9311
César Richard <cesar.richard@weezevent.com>
2021-09-28 11:13:13 +0200

    Adds Gesasso's Mk-Ultra repo

---
+++ github/repositories/gesasso_mkultra.tf
@@ -0,0 +1,43 @@
+### Gesasso Mk Mail
+
+resource "github_repository" "gesasso_mkultra" {
+  allow_merge_commit     = true
+  allow_rebase_merge     = true
+  allow_squash_merge     = true
+  archived               = false
+  auto_init              = false
+  delete_branch_on_merge = true
+  description            = "Le nécessaire pour les mails"
+  has_downloads          = false
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = false
+  name                   = "gesasso_mkmail"
+  visibility             = "public"
+  vulnerability_alerts   = true
+}
+
+resource "github_branch_protection" "gesasso_mkultra_master" {
+  repository_id     = github_repository.gesasso_mkmail.node_id
+  pattern           = "main"
+  push_restrictions = [var.teams.simde.node_id]
+  required_pull_request_reviews {
+    restrict_dismissals             = true
+    dismiss_stale_reviews           = true
+    dismissal_restrictions          = [var.teams.simde.node_id]
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+}
+
+resource "github_team_repository" "simde_gesasso_mkultra" {
+  team_id    = var.teams.simde.id
+  repository = github_repository.gesasso_mkmail.name
+  permission = "maintain"
+}
+
+resource "github_team_repository" "gesasso_gesasso_mkultra" {
+  team_id    = var.teams.gesasso.id
+  repository = github_repository.gesasso_mkmail.name
+  permission = "maintain"
+}
