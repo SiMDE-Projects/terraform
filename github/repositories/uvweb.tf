611e93ba5f580ff62aa83fc501ef097cfcd17559
César Richard <cesar.richard2@gmail.com>
2022-03-09 14:12:18 +0100

    Disabled dangling uvweb repo conf... Added a TODO

--- github/repositories/uvweb.tf
+++ github/repositories/uvweb.tf
@@ -8,32 +8,32 @@ resource "github_repository" "uvweb" {
   archived      = true
 }
 
-
-data "github_branch" "uvweb_master" {
-  repository = github_repository.uvweb.name
-  branch     = "master"
-}
-
-resource "github_branch_default" "uvweb" {
-  repository = github_repository.uvweb.name
-  branch     = data.github_branch.uvweb_master.branch
-}
-
-resource "github_branch_protection_v3" "uvweb_master" {
-  repository     = github_repository.uvweb.name
-  branch         = data.github_branch.uvweb_master.branch
-  enforce_admins = true
-
-  required_status_checks {
-    strict   = true
-    contexts = ["Build and Test"]
-  }
-
-  required_pull_request_reviews {
-    dismissal_teams = [var.teams.simde.slug]
-  }
-
-  restrictions {
-    teams = [var.teams.simde.slug]
-  }
-}
+# TODO: Wait for the repository to be initialized
+#data "github_branch" "uvweb_master" {
+#  repository = github_repository.uvweb.name
+#  branch     = "master"
+#}
+#
+#resource "github_branch_default" "uvweb" {
+#  repository = github_repository.uvweb.name
+#  branch     = data.github_branch.uvweb_master.branch
+#}
+#
+#resource "github_branch_protection_v3" "uvweb_master" {
+#  repository     = github_repository.uvweb.name
+#  branch         = data.github_branch.uvweb_master.branch
+#  enforce_admins = true
+#
+#  required_status_checks {
+#    strict   = true
+#    contexts = ["Build and Test"]
+#  }
+#
+#  required_pull_request_reviews {
+#    dismissal_teams = [var.teams.simde.slug]
+#  }
+#
+#  restrictions {
+#    teams = [var.teams.simde.slug]
+#  }
+#}
