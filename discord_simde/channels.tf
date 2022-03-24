d1284dd02438bb117ec60982699712dced8af2c8
César Richard <cesar.richard2@gmail.com>
2022-03-24 17:33:44 +0100

    Added role & channel "Portail"

--- discord_simde/channels.tf
+++ discord_simde/channels.tf
@@ -151,12 +151,23 @@ module "private-integfev" {
   roles       = local.roles
 }
 
+module "private-portail" {
+  source      = "./channels"
+  name        = "private-portail"
+  api_token   = var.api_token
+  server_id   = discord_server.server.id
+  position    = module.private-integfev.position + 1
+  restricted  = true
+  permissions = local.permissions
+  roles       = local.roles
+}
+
 module "private-admin" {
   source      = "./channels"
   name        = "private-admin"
   api_token   = var.api_token
   server_id   = discord_server.server.id
-  position    = module.private-integfev.position + 1
+  position    = module.private-portail.position + 1
   restricted  = true
   permissions = local.permissions
   roles       = local.roles
