14ff6d4a2c0b02b2b1a28ec947a69e1144449164
César Richard <cesar.richard2@gmail.com>
2022-07-10 15:51:16 +0200

    Added A.Rolland discord member id

--- discord_simde/members.tf
+++ discord_simde/members.tf
@@ -474,12 +474,12 @@ module "leo_mullier" {
   }
 }
 
-#module "alexia_rolland" {
-#  source    = "./member"
-#  api_token = var.api_token
-#  server_id = discord_server.server.id
-#  user_id   = "TBD"
-#  roles     = {
-#    (discord_role.pacha_com.id)    = true
-#  }
-#}
+module "alexia_rolland" {
+  source    = "./member"
+  api_token = var.api_token
+  server_id = discord_server.server.id
+  user_id   = "689494793834725441"
+  roles     = {
+    (discord_role.pacha_com.id)    = true
+  }
+}
