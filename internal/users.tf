5abe2f1a9f7ae891fa72639e063d4f66ee3a203f
César Richard <cesar.richard2@gmail.com>
2023-03-03 17:58:48 +0100

    Updated sql admin privileges

--- internal/users.tf
+++ internal/users.tf
@@ -1,10 +1,26 @@
+## Ghituale
 resource "mysql_user" "ghituale" {
-  user               = "ghituale"
-  host               = "%.mde.utc"
+  user = "ghituale"
+  host = "%.mde.utc"
 }
 
 resource "mysql_grant" "ghituale" {
-  user = mysql_user.ghituale.user
-  database   = "*.*"
-  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "SHOW VIEW", "CREATE", "DROP", "ALTER", "INDEX", "CREATE VIEW"]
+  user       = mysql_user.ghituale.user
+  host       = mysql_user.ghituale.host
+  database   = "*"
+  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "CREATE", "DROP", "INDEX", "ALTER", "CREATE VIEW", "SHOW VIEW"]
+}
+
+## Cerichar
+
+resource "mysql_user" "cerichar" {
+  user = "cerichar"
+  host = "%.mde.utc"
+}
+
+resource "mysql_grant" "cerichar" {
+  user       = mysql_user.cerichar.user
+  host       = mysql_user.cerichar.host
+  database   = "*"
+  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "CREATE", "DROP", "INDEX", "ALTER", "CREATE VIEW", "SHOW VIEW"]
 }
