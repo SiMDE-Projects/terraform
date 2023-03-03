394b2aa1adeda72634c2e9138b88b5f70eace289
César Richard <cesar.richard2@gmail.com>
2023-03-03 18:07:07 +0100

    Added lchappui sql account

--- internal/users.tf
+++ internal/users.tf
@@ -8,7 +8,7 @@ resource "mysql_grant" "ghituale" {
   user       = mysql_user.ghituale.user
   host       = mysql_user.ghituale.host
   database   = "*"
-  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "CREATE", "DROP", "INDEX", "ALTER", "CREATE VIEW", "SHOW VIEW"]
+  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "CREATE", "DROP", "INDEX", "ALTER", "CREATE VIEW", "SHOW VIEW", "DELETE HISTORY"]
 }
 
 ## Cerichar
@@ -22,5 +22,19 @@ resource "mysql_grant" "cerichar" {
   user       = mysql_user.cerichar.user
   host       = mysql_user.cerichar.host
   database   = "*"
-  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "CREATE", "DROP", "INDEX", "ALTER", "CREATE VIEW", "SHOW VIEW"]
+  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "CREATE", "DROP", "INDEX", "ALTER", "CREATE VIEW", "SHOW VIEW", "DELETE HISTORY"]
+}
+
+## lchappuis
+
+resource "mysql_user" "lchappui" {
+  user = "lchappui"
+  host = "%.mde.utc"
+}
+
+resource "mysql_grant" "lchappui" {
+  user       = mysql_user.lchappui.user
+  host       = mysql_user.lchappui.host
+  database   = "*"
+  privileges = ["SELECT", "UPDATE", "INSERT", "DELETE", "CREATE", "DROP", "INDEX", "ALTER", "CREATE VIEW", "SHOW VIEW", "DELETE HISTORY"]
 }
