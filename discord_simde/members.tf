3d9328e0a209840c72d90ff7d3557e0eef6e435c
Léo <91556140+leochapp@users.noreply.github.com>
2023-04-17 09:19:49 +0200

    fix bugs
    victor ronfaut delete on cascade manuel x)

--- discord_simde/members.tf
+++ discord_simde/members.tf
@@ -20,6 +20,14 @@ module "noe_amiot" {
   }
 }
 
+module "victor_ronfaut" {
+  source    = "./member"
+  api_token = var.api_token
+  server_id = discord_server.server.id
+  user_id   = "166621029412765696"
+  roles     = {}
+}
+
 module "maxime_vaillant" {
   source    = "./member"
   api_token = var.api_token
